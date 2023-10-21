a418960a27a97a4f9782f331f39896cfeca763e9
Arth19 <arthur.mbmelo@ufrpe.br>
2023-10-21 18:03:34 -0300

    Swagger API

--- backend/koureia/urls.py
+++ backend/koureia/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from salao.views import (
     FuncionarioViewSet,
@@ -30,6 +32,23 @@ router.register(r'itens-venda', ItemVendaViewSet)
 router.register(r'pagamentos', PagamentoViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Koureia",
+      default_version='v1',
+      description="API para gerenciamento de um salão de beleza",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contato@seusalao.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('admin/', admin.site.urls),  # incluindo o path para o admin
     path('', include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
