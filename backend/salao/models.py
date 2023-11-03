2c12cda6d16098b970e426ecf741f5f1f4d9caa9
Arth19 <arthur.mbmelo@ufrpe.br>
2023-11-03 15:47:43 -0300

    refactor some models and tests

--- backend/salao/models.py
+++ backend/salao/models.py
@@ -1,29 +1,53 @@
-# models.py
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Funcionario(AbstractUser):
-    telefone = models.CharField(max_length=15)
-    # Outros campos específicos dos funcionários
+    telefone = PhoneNumberField(blank=False, unique=True)
+
+    def __str__(self):
+        return self.username
 
 class CategoriaServico(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.nome
 
 class Servico(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, db_index=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(CategoriaServico, on_delete=models.SET_NULL, null=True)
 
+    def clean(self):
+        if self.preco < 0:
+            raise ValidationError({"preco": "O preço não pode ser negativo."})
+
+    def __str__(self):
+        return self.nome
+
 class Cliente(models.Model):
-    nome = models.CharField(max_length=255)
-    telefone = models.CharField(max_length=15)
+    nome = models.CharField(max_length=255, db_index=True)
+    telefone = PhoneNumberField(blank=False, unique=True)
     data_nascimento = models.DateField()
 
+    def __str__(self):
+        return self.nome
+
 class Agendamento(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
-    funcionario = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, db_index=True)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE, db_index=True)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, db_index=True)
     data_hora = models.DateTimeField()
+
+    def clean(self):
+        if self.data_hora < timezone.now():
+            raise ValidationError({"data_hora": "A data e hora do agendamento não podem estar no passado."})
+
+    def __str__(self):
+        return f"Agendamento de {self.cliente} para {self.servico} com {self.funcionario} em {self.data_hora}"
 
 class Promocao(models.Model):
     nome = models.CharField(max_length=255)
@@ -32,29 +56,68 @@ class Promocao(models.Model):
     inicio_validade = models.DateTimeField()
     fim_validade = models.DateTimeField()
 
+    def clean(self):
+        if self.inicio_validade >= self.fim_validade:
+            raise ValidationError({"fim_validade": "A data de fim deve ser posterior à data de início."})
+
+    def __str__(self):
+        return self.nome
+
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    quantidade_estoque = models.PositiveIntegerField()
+    quantidade_estoque = models.PositiveIntegerField(default=0)
+
+    def clean(self):
+        if self.preco < 0:
+            raise ValidationError({"preco": "O preço não pode ser negativo."})
+
+    def __str__(self):
+        return self.nome
 
 class Venda(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
     data_hora = models.DateTimeField()
     total = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"Venda para {self.cliente} em {self.data_hora}"
+
 class ItemVenda(models.Model):
     venda = models.ForeignKey(Venda, related_name='itens', on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField()
     preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def clean(self):
+        if self.quantidade <= 0:
+            raise ValidationError({"quantidade": "A quantidade deve ser um valor positivo."})
+
+    def __str__(self):
+        return f"Item de {self.produto} na venda {self.venda.id}"
 
 class Pagamento(models.Model):
     venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
     data_hora = models.DateTimeField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"Pagamento de {self.valor} para a venda {self.venda.id}"
+
 class Avaliacao(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
-    servico = models.ForeignKey(Servico, on_delete=models.SET_NULL, null=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True, db_index=True)
+    servico = models.ForeignKey(Servico, on_delete=models.SET_NULL, null=True, db_index=True)
     avaliacao = models.PositiveIntegerField()
     comentario = models.TextField(null=True, blank=True)
+
+    class Meta:
+        unique_together = (('cliente', 'servico'),)
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+
+    def clean(self):
+        if not 1 <= self.avaliacao <= 5:
+            raise ValidationError({"avaliacao": "A avaliação deve estar entre 1 e 5."})
+
+    def __str__(self):
+        return f"Avaliação de {self.cliente} para {self.servico}"
--- backend/salao/tests.py
+++ backend/salao/tests.py
@@ -1,5 +1,6 @@
-# salao/tests.py
-
+import datetime
+from django.utils import timezone  # Certifique-se de importar timezone assim
+from django.forms import ValidationError
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
@@ -35,8 +36,8 @@ from .serializers import (
 class ModelTestCase(TestCase):
 
     def setUp(self):
-        self.funcionario = Funcionario.objects.create(username='func1', password='senha123')
-        self.cliente = Cliente.objects.create(nome='Cliente Teste', telefone='123456789', data_nascimento='1990-01-01')
+        self.funcionario = Funcionario.objects.create(username='func1', password='senha123', telefone='+5511912345678')
+        self.cliente = Cliente.objects.create(nome='Cliente Teste', telefone='+5511987654321', data_nascimento='1990-01-01')
         self.categoria_servico = CategoriaServico.objects.create(nome='Corte')
         self.servico = Servico.objects.create(nome='Corte de Cabelo', preco='50.00', categoria=self.categoria_servico)
 
@@ -53,13 +54,28 @@ class ModelTestCase(TestCase):
         agendamento = Agendamento.objects.create(cliente=self.cliente, servico=self.servico, funcionario=self.funcionario, data_hora='2023-10-22T10:00')
         self.assertEqual(agendamento.cliente.nome, 'Cliente Teste')
 
+    def test_invalid_service_price(self):
+        with self.assertRaises(ValidationError):
+            servico_invalido = Servico.objects.create(nome='Serviço Inválido', preco='-50.00', categoria=self.categoria_servico)
+            servico_invalido.full_clean()
+
+    def test_invalid_agendamento_date(self):
+        with self.assertRaises(ValidationError):
+            agendamento_invalido = Agendamento.objects.create(
+                cliente=self.cliente,
+                servico=self.servico,
+                funcionario=self.funcionario,
+                data_hora=timezone.make_aware(datetime.datetime(2022, 10, 22, 10, 0))  # Agora usando datetime.datetime
+            )
+            agendamento_invalido.full_clean()
+
 # Testes para a API
 @override_settings(REST_FRAMEWORK={'DEFAULT_PERMISSION_CLASSES': []})
 class APITestCase(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.funcionario = Funcionario.objects.create_superuser(username='func1', password='senha123')  # alterado para create_superuser
+        self.funcionario = Funcionario.objects.create_superuser(username='func1', password='senha123', telefone='+5511912345678')  # número de telefone atualizado
         self.client.force_authenticate(user=self.funcionario)
 
     def test_listagem_funcionarios(self):
@@ -71,9 +87,8 @@ class APITestCase(APITestCase):
         data = {
             'username': 'func2',
             'password': 'senha123',
-            'telefone': '123456789'  # Adicionado campo telefone
+            'telefone': '+5511987654321'  # número de telefone atualizado
         }
         response = self.client.post(reverse('funcionario-list'), data, format='json')
         print(response.data)  # Adicionada linha para imprimir a resposta
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
