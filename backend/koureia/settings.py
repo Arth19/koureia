f5046080f31d4bee7da7fbc00d014bec99aecfa5
Arth19 <arthur.mbmelo@ufrpe.br>
2023-11-04 10:48:51 -0300

    removes secret key from the code

--- backend/koureia/settings.py
+++ backend/koureia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_@a$9pk(1*r)(8uyv-6r^gjhp5=idy@tw3k8gn@@4dqo#lau%g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
